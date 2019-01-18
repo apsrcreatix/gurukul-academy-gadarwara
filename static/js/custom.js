eb9a464baaa5148deb69eaa7a008d0cfc1b2e264
Aditya Pratap Singh Rajput <apsrcreatix@gmail.com>
2019-01-19 00:27:54 +0530

    add content

--- static/js/custom.js
+++ static/js/custom.js
@@ -23,8 +23,19 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   
   });
-  var carousels = bulmaCarousel.attach(); // carousels now contains an array of all Carousel instances
-ß
+ // When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
 
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
 
- 
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
